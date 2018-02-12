cb0d76785ac02e41e1c4005e359269ceab29557e
Lacy Morrow <me@lacymorrow.com>
2018-02-12 11:48:15 -0800

    unbrand

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -5,7 +5,6 @@ import Typography from 'views/Typography/Typography';
 import Icons from 'views/Icons/Icons';
 import Maps from 'views/Maps/Maps';
 import Notifications from 'views/Notifications/Notifications';
-import Upgrade from 'views/Upgrade/Upgrade';
 
 const appRoutes = [
     { path: "/dashboard", name: "Dashboard", icon: "pe-7s-graph", component: Dashboard },
@@ -15,7 +14,6 @@ const appRoutes = [
     { path: "/icons", name: "Icons", icon: "pe-7s-science", component: Icons },
     { path: "/maps", name: "Maps", icon: "pe-7s-map-marker", component: Maps },
     { path: "/notifications", name: "Notifications", icon: "pe-7s-bell", component: Notifications },
-    { upgrade: true, path: "/upgrade", name: "Upgrade to PRO", icon: "pe-7s-rocket", component: Upgrade },
     { redirect: true, path:"/", to:"/dashboard", name: "Dashboard" }
 ];
 
