7fc58010e9d7254426ceb61242faada542533a4e
Lacy Morrow <me@lacymorrow.com>
2018-02-20 16:48:31 -0800

    got ticker working

--- components/elements/Ticker.js
+++ components/elements/Ticker.js
@@ -9,41 +9,30 @@ import fetch from 'isomorphic-unfetch'
 import { colors } from '../styles'
 import Card from '../Card'
 
-const getJson = ( url ) => {
-
- 	const proxy = 'https://peacecors.herokuapp.com/'
- 	return fetch( proxy + url ).then( res => res.json() ).catch( console.log )
-
-}
 const lookup = ( symbol ) => new Promise( ( resolve, reject ) => {
 
-	 Promise.all( [
-					 getJson( `https://autoc.finance.yahoo.com/autoc?query=${symbol}&region=1&lang=en` ),
-					 getJson( `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?&modules=summaryProfile,financialData` )
-			 ] )
-			 .then( ( responses ) => {
+	 const proxy = 'https://peacecors.herokuapp.com/'
+	 fetch( proxy + `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?&modules=summaryProfile,financialData` )
+	 .then( res => res.json() )
+	 .then( res => {
 
-					 console.log( responses )
-					 if ( !responses[0].ResultSet.Result.length ) {
+		const financialData = res.quoteSummary.result[0].financialData
+		  if ( !financialData ) {
 
-							 reject( true )
-							 return
+		 		 reject( {} )
 
-			}
-					 const financialData = responses[1].quoteSummary.result[0].financialData
-					 resolve( {
-							 symbol,
-							 name: responses[0].ResultSet.Result[0].name,
-							 exchange: responses[0].ResultSet.Result[0].exchDisp,
-							 currentPrice: financialData.currentPrice.raw,
-							 highPrice: financialData.targetHighPrice.raw,
-							 lowPrice: financialData.targetLowPrice.raw,
-							 meanPrice: financialData.targetMeanPrice.raw,
-							 medianPrice: financialData.targetMedianPrice.raw
-					 } )
+		}
+		 resolve( {
+				 symbol,
+				 currentPrice: financialData.currentPrice.raw,
+				 highPrice: financialData.targetHighPrice.raw,
+				 lowPrice: financialData.targetLowPrice.raw,
+				 meanPrice: financialData.targetMeanPrice.raw,
+				 medianPrice: financialData.targetMedianPrice.raw
+		 } )
 
 		} )
-			 .catch( reject )
+		 .catch( reject )
 
 } )
 
@@ -53,60 +42,38 @@ class Ticker extends React.Component {
 
 		super( props )
 		this.state = {
-			currentTime: new Date(),
 			symbol: props.symbol || 'AAPL',
-			ticker: {}
+			ticker: {
+				currentPrice: '--'
+
+			}
 		}
 
 	}
 	componentDidMount () {
 
-		lookup( this.state.symbol )
+		this.fetchRequest = lookup( this.state.symbol )
 			.then( ( res ) => {
-
-				console.log( 'final: ', res )
 				this.setState( {ticker: res} )
-
 			} )
-
-		this.setState( {
-			currentTime: new Date()
-		}, this.updateTime )
 
 	}
 	componentWillUnmount () {
 
-		if ( this.timerId ) {
-
-			clearTimeout( this.timerId )
-
-		}
-		typeof this.fetchRequest === 'function' && this.fetchRequest.cancel()
-
-	}
-
-	updateTime ( e ) {
-
-		this.timerId = setTimeout( () => {
-
-			this.setState( {
-				currentTime: new Date()
-			}, this.updateTime )
-
-		}, 3600000 )
+		typeof this.fetchRequest.cancel === 'function' && this.fetchRequest.cancel()
 
 	}
 
 	render () {
 
-		const { currentTime, symbol, ticker } = this.state
+		const { symbol, ticker } = this.state
 
 		return (
 			<Card
 				className='ticker'
 				title={this.state.ticker.symbol}
-				text={this.state.ticker.currentPrice}
-				subtext={this.state.ticker.exchange}
+				text={'' + this.state.ticker.currentPrice}
+				subtext={'AVG: ' + this.state.ticker.meanPrice}
 				{...this.props} />
 		)
 
