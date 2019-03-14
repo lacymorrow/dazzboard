39ecf2e6841a67ddb937ec4749ee745f0d8c7d08
Lacy Morrow <me@lacymorrow.com>
2019-03-14 13:41:02 -0700

    proton-native

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
 
-          <title>Init</title>
+          <title>Dazzboard</title>
 
           {this.props.styleTags}
         </Head>
